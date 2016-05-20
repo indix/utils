02a9238ccb65105b3cdbf710ecf4f3df4482996d
Azhagu Selvan SP <tamizhgeek@gmail.com>
2016-05-20 16:06:30 +0530

    Added script to build docs and push to gh-pages

--- bin/build_docs.sh
+++ bin/build_docs.sh
@@ -6,11 +6,10 @@ BUILD_FOLDER="build/"
 
 
 if [ ! -d "$BUILD_FOLDER" ]; then
-  git clone -b master git@github.com:ind9/utils $DEPLOY_FOLDER
+  git clone git@github.com:ind9/utils $BUILD_FOLDER
 fi
 
 cd $BUILD_FOLDER
-git pull origin gh-pages
 
 sbt unidoc ghpagesPushSite
 
