5480261604f3ebee8600b42c3c6403facc0cad3d
Azhagu Selvan SP <tamizhgeek@gmail.com>
2016-05-23 14:56:40 +0530

    Added all the urlUtils from the force repo

---
+++ util-core/src/main/scala/com/indix/utils/core/UrlUtils.scala
@@ -0,0 +1,70 @@
+package com.indix.utils.core
+
+import java.net.{URI, URLDecoder, URL}
+
+import scala.util.Try
+
+/** Useful helper methods to operate on product urls */
+object UrlUtils {
+
+  /** extract hostname from a given url
+    *
+    * @param url Input url
+    * @return Extracted hostname
+    *
+    */
+  def toHostname(url: String): Option[String] = Try(new URL(url)).map(_.getHost.toLowerCase).toOption
+
+  /** Find if the url is valid one or not
+    *
+    * @param url Input ulr
+    * @return if the url is valid or not
+    */
+  def isValid(url: String): Boolean = {
+    try { new URL(url).getHost; true } catch { case _: Throwable => false }
+  }
+
+  /**
+    *
+    * @param pageUrl The base url. ie the url of the page
+    * @param extractedUrl Url extracted from the page. Mostly a relative url segment
+    * @return The extractedUrl segment resolved against the base url of the page
+    */
+  def resolve(pageUrl: String, extractedUrl: String): String = {
+    URI.create(pageUrl).normalize().resolve(extractedUrl).normalize().toString
+  }
+
+  /** Decode a given url using UTF-8 encoding
+    *
+    * @param url Input url
+    * @return Decoded url
+    */
+  def decode(url: String) = URLDecoder.decode(url, "UTF-8")
+
+
+  /**
+    *
+    * @param url Input url
+    * @return URL query params(key-value pairs) as [[Map]]
+    */
+  def toQueryMap(url: String): Map[String, String] = {
+    val query = try {
+      Option(new URL(url).getQuery).getOrElse("")
+    } catch {
+      case e: Throwable => ""
+    }
+    queryStringMap(query)
+  }
+
+  private def queryStringMap(query: String) = {
+    val parts = query.split("&")
+    parts.filter(_.nonEmpty).map {
+      p =>
+        val q = p.split("=")
+        q.length match {
+          case 2 => URLDecoder.decode(q(0), "UTF-8") -> URLDecoder.decode(q(1), "UTF-8")
+          case 1 => URLDecoder.decode(q(0), "UTF-8") -> ""
+        }
+    }.toMap
+  }
+}
---
+++ util-core/src/test/scala/com/indix/utils/core/UrlUtilsSpec.scala
@@ -0,0 +1,43 @@
+package com.indix.utils.core
+
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers._
+
+import com.indix.utils.core.UrlUtils
+
+class UrlUtilsSpec extends FlatSpec {
+
+  "UrlUtils#toHostname" should "return the hostname of any given url" in {
+    UrlUtils.toHostname("http://www.google.com") should be(Some("www.google.com"))
+    UrlUtils.toHostname("https://www.google.com") should be(Some("www.google.com"))
+    UrlUtils.toHostname("www.google.com/abc") should be(None)
+  }
+
+  "UrlUtils#toQueryMap" should "return the query params from a url as a scala map" in {
+    val resMap = UrlUtils.toQueryMap("http://google.com/?query=hello&lang=en&somekey=value&")
+    resMap.size should be (3)
+    resMap.head should be ("query", "hello")
+
+    val resMap1 = UrlUtils.toQueryMap("http://google.com/???query=hello&lang=en&somekey=value&")
+    resMap1.size should be (3)
+    resMap1.head should be ("??query", "hello")
+
+    val resMap2 = UrlUtils.toQueryMap("http://google.com/")
+    resMap2.size should be (0)
+  }
+
+  "UrlUtils#isValid" should "return true/false given the url is valid" in {
+    UrlUtils.isValid("google.coma") should be(false)
+    UrlUtils.isValid("https://google.com") should be(true)
+  }
+
+  "UrlUtils#resolve" should "resolve relative urls against the base url" in {
+    UrlUtils.resolve("http://google.com/", "shopping") should be("http://google.com/shopping")
+  }
+
+  "UrlUtils#decode" should "UTF-8 encoded urls to unicode strings" in {
+    UrlUtils.decode("http%3A%2F%2Fwww.example.com%2Fd%C3%BCsseldorf%3Fneighbourhood%3DL%C3%B6rick") should be ("http://www.example.com/düsseldorf?neighbourhood=Lörick")
+  }
+
+}
